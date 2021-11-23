bc2cc1d42769d4adff7fe90b2b27c11be971c5c0
AvioPro <nani.downhill@gmail.com>
2021-11-23 16:39:01 +0200

    First Commit of Doctor app

---
+++ DoctorApp/Models/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+namespace DoctorApp.Models
+{
+    public class ApplicationDbContext:IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
